a302dc932f1b97286024ef25db4eecb4d7c93263
Johnny Reina <johnny.r.reina@gmail.com>
2021-10-04 02:05:32 -0500

    The observer pattern but different and worse

--- ShittyPromise.Tests/PromiseTests.cs
+++ ShittyPromise.Tests/PromiseTests.cs
@@ -1,14 +1,21 @@
 using System;
 using Xunit;
+using ShittyPromise;
 
 namespace ShittyPromise.Tests
 {
     public class PromiseTests
     {
         [Fact]
-        public void Test1()
+        public void Should_resolve_syncronous_action()
         {
+            var p = new Promise<int>((res, rej) => {
+                res(100);
+            });
 
+            p.Then<int>(x => {
+                Assert.Equal(100, x);
+            });
         }
     }
 }
